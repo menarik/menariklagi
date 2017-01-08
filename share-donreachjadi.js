78d7f419483bdaed620c8c82ad39aa24ecaee92f
menarik <menarikblass@gmail.com>
2017-01-08 14:48:40 +0700

    Create share-donreachjadi.js

---
+++ share-donreachjadi.js
@@ -0,0 +1,7 @@
+/*! buttons v1.1.4 - 2016-05-01 | Copyright (c) 2016 Adam Bouqdib (http://abemedia.co.uk) | donreach.com/license */
+ (function() {
+    var dr = document.createElement('script');
+    dr.type = 'text/javascript'; dr.async = true;
+    dr.src = '//cdn.rawgit.com/menarik/menariklagi/master/donreachasli.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
+  })();
